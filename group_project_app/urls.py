2d4651708d9fef0639c8defa2d93c6795578aec0
Kevin Cole <kfcole20@gmail.com>
2021-09-20 13:26:39 -0500

    added styling

--- group_project_app/urls.py
+++ group_project_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index),
+    path('register',views.register),
+    path('main', views.main),
+    path('login', views.login),
+    path('logout', views.logout)
 ]
